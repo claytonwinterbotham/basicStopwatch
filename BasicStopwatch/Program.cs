8c247d8e09e4977297150a38b8e639904c3e8fbd
Clayton Winterbotham <cwinterbotham@gmail.com>
2018-05-27 16:01:28 -0700

    first commit

---
+++ BasicStopwatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicStopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch(); //Instantiate stopwatch object
+
+            var message = "Type start to begin watch";
+
+            //While loop allows user to continually toggle between start and stopping the watch until they type exit.
+            while (true)
+            {
+                Console.WriteLine(message);
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "start":
+                        stopwatch.StartWatch();
+                        message = "Type stop to end watch";
+                        break;
+                    case "stop":
+                        stopwatch.EndWatch();
+                        Console.WriteLine(stopwatch.DisplayTime());
+                        message = "Type start to begin watch, or exit to quit application.";
+                        break;
+                }
+
+                if(input == "exit")
+                {
+                    break;
+                }
+                
+            } 
+        }
+    }
+}
---
+++ BasicStopwatch/Stopwatch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BasicStopwatch
+{
+    class Stopwatch
+    {
+        private DateTime _startTime;
+        private int _endTime;
+        private bool _hasStarted = false;
+
+        //Method will set start time to current time, or print error message if watch is already started
+        public void StartWatch()
+        {
+            if (_hasStarted)
+            {
+                Console.WriteLine("Watch has already started. Must enter stop first");
+            }
+            else
+            {
+                _startTime = DateTime.Now;
+                _hasStarted = true;
+            }
+           
+        }
+
+        //Method will calculate end time, or print error message if clock is already stopped
+        public void EndWatch()
+        {
+            if (!_hasStarted)
+            {
+                Console.WriteLine("Watch is already stopped. Must enter start first");
+            }
+            else
+            {
+                var timeSpan = DateTime.Now - _startTime;
+                _endTime = timeSpan.Seconds;
+                _hasStarted = false;
+            }
+            
+        }
+
+        //Method will format time duration into readable string
+        public string DisplayTime()
+        {
+            TimeSpan t = TimeSpan.FromSeconds(_endTime); //Use timespan class to help format string
+            string time = string.Format("{0:D2}h:{1:D2}m:{2:D2}s",
+                            t.Hours,
+                            t.Minutes,
+                            t.Seconds);
+            return time;
+        }
+
+    }
+}
